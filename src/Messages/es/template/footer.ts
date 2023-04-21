6bdd992cb19ada59e86035b1c124ebe4fafc33c1
SergioYepes <100533053+SergioYepes@users.noreply.github.com>
2023-04-21 10:01:30 -0500

    fix: footer changes

--- src/Messages/es/template/footer.ts
+++ src/Messages/es/template/footer.ts
@@ -4,6 +4,6 @@ export const footerSpanish = {
   "footer-links-of-interest": "Enlaces de interés",
   "footer-data-policy": "Polítca de datos",
   "footer-about-us": "Contáctanos",
-  "footer-email": "Correo : hello@itglobers.com",
+  "footer-email": "Correo : ",
   "footer-rights-reserved": "Todos los derechos reservados",
 };
